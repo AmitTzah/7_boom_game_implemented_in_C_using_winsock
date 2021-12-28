95e7d256e9f1960e26b77ee3484b620cfa7a394d
AmitTzah <93420076+AmitTzah@users.noreply.github.com>
2021-12-28 16:47:53 +0200

    Added client_log file and functions to write the connection_succeeded_and_failed_messages

--- client/main.c
+++ client/main.c
@@ -29,11 +29,11 @@ Project: Ex4
 #pragma comment(lib,"WS2_32")
 
 
-int reconnect_or_exit(SOCKET m_socket, const struct sockaddr* name, int namelen);
+int reconnect_or_exit(SOCKET m_socket, const struct sockaddr* name, int namelen, int illegal_command);
 void get_path_to_log_file(char* path_to_log_file, char* client_name);
 void get_connection_succeeded_and_failed_messages(char* connection_succeeded_message, char* connection_failed_message, char* ip, char* port);
 
-int bytes_offset_to_log_file;
+int write_from_offset_to_log_file;
 char client_log_file_name[MAX_LENGTH_OF_PATH_TO_A_FILE];
 
 char connection_succeeded_message[MAX_LENGH_OF_INPUT_FROM_USER];
@@ -41,14 +41,14 @@ char connection_failed_message[MAX_LENGH_OF_INPUT_FROM_USER];
 
 void main(int argc, char* argv[]) {
 	SOCKADDR_IN clientService;
-	
-	bytes_offset_to_log_file = 0;
+	int tried_to_reconnect = 0;
+	write_from_offset_to_log_file = 0;
 	get_path_to_log_file(client_log_file_name, argv[3]);
 	printf("the client_log_file_name is: %s\n", client_log_file_name);
 
 	get_connection_succeeded_and_failed_messages(connection_succeeded_message, connection_failed_message, argv[1], argv[2]);
-	printf("the connection_succeeded_message is: %s\n", connection_succeeded_message);
-	printf("the connection_failed_message is: %s\n", connection_failed_message);
+	printf("the connection_succeeded_message is: %s", connection_succeeded_message);
+	printf("the connection_failed_message is: %s", connection_failed_message);
 
 
 	// Initialize Winsock.
@@ -75,8 +75,8 @@ void main(int argc, char* argv[]) {
 	clientService.sin_port = htons(strtol(argv[2],NULL, 10)); //Setting the port to connect to.
 
 	if (connect(m_socket, (SOCKADDR*)&clientService, sizeof(clientService)) == SOCKET_ERROR) {
-			
-		if (1 == reconnect_or_exit(m_socket, (SOCKADDR*)&clientService, sizeof(clientService))) {
+		tried_to_reconnect = 1;
+		if (1 == reconnect_or_exit(m_socket, (SOCKADDR*)&clientService, sizeof(clientService),0)) {
 			//user chose to exit.
 			goto client_cleanup;
 
@@ -85,7 +85,11 @@ void main(int argc, char* argv[]) {
 		//else, reconnected!
 	}
 
-	printf("%s\n", connection_succeeded_message);
+	if (tried_to_reconnect != 1) {
+		WinWriteToFile(client_log_file_name, connection_succeeded_message, strlen(connection_succeeded_message), write_from_offset_to_log_file);
+		printf("%s", connection_succeeded_message);
+		write_from_offset_to_log_file += strlen(connection_succeeded_message);
+	}
 
 	int bytes_sent = send(m_socket, "hi", 1+ strlen("hi"), 0);
 
@@ -105,12 +109,17 @@ client_cleanup:
 
 
 // trying to reconnect recursively.
+// if illegal_command=1, then the function was called user entered a wrong input, and connection failed messeage won't be print to screen and file.
 //Return 1 if user chose to Exit.
 //return 0 if succeeded to reconnect.
-int reconnect_or_exit(SOCKET m_socket, const struct sockaddr* name, int namelen) {
+int reconnect_or_exit(SOCKET m_socket, const struct sockaddr* name, int namelen, int illegal_command) {
 	char choice[MAX_LENGH_OF_INPUT_FROM_USER];
 
-	printf("%s\n", connection_failed_message);
+	if (illegal_command == 0) {
+		printf("%s", connection_failed_message);
+		WinWriteToFile(client_log_file_name, connection_failed_message, strlen(connection_failed_message), write_from_offset_to_log_file);
+		write_from_offset_to_log_file += strlen(connection_failed_message);
+	}
 	printf("Choose what to do next:\n");
 	printf("1. Try to reconnect\n");
 	printf("2. Exit\n");
@@ -119,12 +128,15 @@ int reconnect_or_exit(SOCKET m_socket, const struct sockaddr* name, int namelen)
 	if (choice[0] == '1') {
 		if (connect(m_socket, name, namelen) == SOCKET_ERROR) {
 
-			return reconnect_or_exit(m_socket, name, namelen);
+			return reconnect_or_exit(m_socket, name, namelen,0);
 
 		}
 
 		else {
-			//WinWriteToFile(client_log_file_name,)
+			printf("%s", connection_succeeded_message);
+			WinWriteToFile(client_log_file_name, connection_succeeded_message, strlen(connection_succeeded_message), write_from_offset_to_log_file);
+			write_from_offset_to_log_file += strlen(connection_succeeded_message);
+
 			return 0;
 
 		}
@@ -139,7 +151,7 @@ int reconnect_or_exit(SOCKET m_socket, const struct sockaddr* name, int namelen)
 	else {
 
 		printf("Error: illegal command\n");
-		return reconnect_or_exit(m_socket, name, namelen);
+		return reconnect_or_exit(m_socket, name, namelen,1);
 	}
 
 }
@@ -152,6 +164,7 @@ void get_path_to_log_file(char* path_to_log_file, char* client_name) {
 
 
 }
+
 void get_connection_succeeded_and_failed_messages(char* connection_succeeded_message, char* connection_failed_message, char* ip, char* port) {
 
 	
@@ -159,10 +172,13 @@ void get_connection_succeeded_and_failed_messages(char* connection_succeeded_mes
 	strcat_s(connection_succeeded_message, MAX_LENGH_OF_INPUT_FROM_USER, ip);
 	strcat_s(connection_succeeded_message, MAX_LENGH_OF_INPUT_FROM_USER, ":");
 	strcat_s(connection_succeeded_message, MAX_LENGH_OF_INPUT_FROM_USER, port);
+	strcat_s(connection_succeeded_message, MAX_LENGH_OF_INPUT_FROM_USER, "\n");
+
 
 	strcpy_s(connection_failed_message, MAX_LENGH_OF_INPUT_FROM_USER, "Failed connecting to server on ");
 	strcat_s(connection_failed_message, MAX_LENGH_OF_INPUT_FROM_USER, ip);
 	strcat_s(connection_failed_message, MAX_LENGH_OF_INPUT_FROM_USER, ":");
 	strcat_s(connection_failed_message, MAX_LENGH_OF_INPUT_FROM_USER, port);
+	strcat_s(connection_failed_message, MAX_LENGH_OF_INPUT_FROM_USER, "\n");
 
 }
