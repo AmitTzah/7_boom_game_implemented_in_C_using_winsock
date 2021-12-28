bdf3da883ff21d7b420b9be01d1c9574a6a2f195
AmitTzah <93420076+AmitTzah@users.noreply.github.com>
2021-12-28 11:07:19 +0200

    Add project files.

---
+++ Share/HardCodedData.h
@@ -0,0 +1,22 @@
+//This header file holds different macros and structures used in the project.
+
+#ifndef HARD_CODED_DATA_H
+#define HARD_CODED_DATA_H
+
+//general constants
+#define TIMEOUT_IN_MILLISECONDS 5000
+#define BRUTAL_TERMINATION_CODE 0x55
+#define ERROR_CODE ((int)(-1))
+#define SUCCESS_CODE ((int)(0))
+#define MAX_LENGTH_OF_PATH_TO_A_FILE 300
+
+
+
+
+//project specific constants
+
+#define MAX_LENGTH_OF_ROW 200
+#define THREAD_TIMEOUT_IN_MS 80
+
+
+#endif
---
+++ client/main.c
@@ -0,0 +1,25 @@
+/*
+Authors:
+
+Amit Tzah 316062959
+Tomer Shimshi 203200480
+
+
+Project: Ex3
+*/
+
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <Windows.h>
+#include <math.h>
+#include <string.h>
+#include <stdbool.h>
+
+
+#include "create_and_handle_processes.h"
+#include "HardCodedData.h"
+#include "file_IO.h"
+
+
+int main(int argc, char* argv[]) {}
