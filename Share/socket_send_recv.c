6e0269808d696da715a63b1f973eb67fdcb207fa
AmitTzah <93420076+AmitTzah@users.noreply.github.com>
2021-12-29 12:46:40 +0200

    Added format_communication_message() function.

--- Share/socket_send_recv.c
+++ Share/socket_send_recv.c
@@ -1,9 +1,11 @@
 //This module holds functions that wrap winsock2.h api functions for the application-level communication protocol.
+//includes formating for communication messeges, which is project-specific.
 
 #include "socket_send_recv.h"
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 /**
  * SendBuffer() uses a socket to send a buffer.
@@ -180,4 +182,94 @@ TransferResult_t ReceiveString(char** OutputStrPtr, SOCKET sd)
 	}
 
 	return RecvRes;
+}
+
+//returns a pointer to a heap-allocated string of the formated message, according to messeage type and parameters array.
+//the string should be freed in caller 
+char* format_communication_message(const char* messeage_type, char* parameters_array[MAX_NUM_OF_MESSAGE_PARAMETERS]) {
+	char* communication_message = NULL;
+	int size_of_communication_message = 0;
+	//check if message has three parameters.
+	if (strcmp(messeage_type, "GAME_VIEW") == 0) {
+		size_of_communication_message += 5; //semicolons, colons,newline char , nullterminator for easier use in printf.
+		size_of_communication_message += strlen("GAME_VIEW");
+		for (int j = 0; j < MAX_NUM_OF_MESSAGE_PARAMETERS; j++) {
+			size_of_communication_message += strlen(parameters_array[j]);
+		}
+
+		communication_message = (char*)calloc(size_of_communication_message, sizeof(char));
+		if (communication_message == NULL) {
+			printf("Memory allocation failed in format_communication_message(), exiting...");
+			exit(1);
+
+		}
+
+		else {
+
+			strcpy_s(communication_message, size_of_communication_message, "GAME_VIEW");
+			strcat_s(communication_message, size_of_communication_message, ":");
+			strcat_s(communication_message, size_of_communication_message, parameters_array[0]);
+			strcat_s(communication_message, size_of_communication_message, ";");
+			strcat_s(communication_message, size_of_communication_message, parameters_array[1]);
+			strcat_s(communication_message, size_of_communication_message, ";");
+			strcat_s(communication_message, size_of_communication_message, parameters_array[2]);
+			strcat_s(communication_message, size_of_communication_message, "\n");
+
+		}
+	}
+
+
+	//check if message has only one parameter
+	else if (strcmp(messeage_type, "CLIENT_PLAYER_MOVE") == 0 || strcmp(messeage_type, "CLIENT_REQUEST") == 0 ||
+		strcmp(messeage_type, "TURN_SWITCH") == 0 || strcmp(messeage_type, "GAME_ENDED") == 0) {
+
+		size_of_communication_message += 3;//colons,newline char , nullterminator for easier use in printf.
+		size_of_communication_message += strlen(parameters_array[0]);
+		size_of_communication_message += strlen(messeage_type);
+
+		communication_message = (char*)calloc(size_of_communication_message, sizeof(char));
+		if (communication_message == NULL) {
+			printf("Memory allocation failed in format_communication_message(), exiting...");
+			exit(1);
+
+		}
+
+		else {
+			strcpy_s(communication_message, size_of_communication_message, messeage_type);
+			strcat_s(communication_message, size_of_communication_message, ":");
+			strcat_s(communication_message, size_of_communication_message, parameters_array[0]);
+			strcat_s(communication_message, size_of_communication_message, "\n");
+		}
+	}
+
+	//message has no parameters.
+	else {
+		size_of_communication_message += strlen(messeage_type) + 2;
+		communication_message = (char*)calloc(size_of_communication_message, sizeof(char));
+		if (communication_message == NULL) {
+			printf("Memory allocation failed in format_communication_message(), exiting...");
+			exit(1);
+
+		}
+
+		else {
+			strcpy_s(communication_message, size_of_communication_message, messeage_type);
+			strcat_s(communication_message, size_of_communication_message, "\n");
+		}
+
+	}
+
+
+	return communication_message;
+}
+
+int get_size_of_communication_message(char* communication_message) {
+	int j = 0;
+	while (communication_message[j] != '\n') {
+
+		j++;
+
+	}
+
+	return (j + 1);
 }
--- Share/socket_send_recv.h
+++ Share/socket_send_recv.h
@@ -1,11 +1,14 @@
 //This module holds functions that wrap winsock2.h api functions for the application-level communication protocol.
+//includes formating for communication messeges, which is project-specific.
 
 
 #ifndef SOCKET_SEND_RECV_H
 #define SOCKET_SEND_RECV_H
 
-
 #include <winsock2.h>
+
+#include "HardCodedData.h"
+
 #pragma comment(lib, "ws2_32.lib")
 
 
@@ -23,6 +26,7 @@ TransferResult_t ReceiveBuffer(char* OutputBuffer, int RemainingBytesToReceive, 
 
 TransferResult_t ReceiveString(char** OutputStrPtr, SOCKET sd);
 
+char* format_communication_message(const char* messeage_type, char* parameters_array[MAX_NUM_OF_MESSAGE_PARAMETERS]);
 
 
 #endif
